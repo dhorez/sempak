968ca24cfc8321eb5991307b837fd67e36a74b70
dhorez <31201852+dhorez@users.noreply.github.com>
2017-08-21 14:31:22 +0700

    Update sukses.sh

--- sukses.sh
+++ sukses.sh
@@ -1,42 +1,28 @@
 echo Updating and upgrading your System..
-
 apt-get update
-
 echo Installing desktop #(xrdp)
-
 apt-get install xorg xrdp lxde -y
 apt-get install nano -y
-
-
 echo Removing Screensaver
 apt-get remove xscreensaver -y
-
 #echo Install Htop
 #apt-get install htop -y
 #
-
 #echo Installing Chromium-Browser
 #
 #apt-get install chromium-browser -y
-
 apt-get install firefox -y
-
 #wget https://github.com/mazbron/x/raw/master/iMacros-new.tar.gz
 #tar -xvf iMacros-new.tar.gz
-
 echo install chrome
-
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update -y
 sudo apt-get install google-chrome-stable -y
-
 sudo apt-get install flashplugin-installer -y
-
 wget https://github.com/mazbron/x/raw/master/install_flash_player_11_linux.x86_64.tar.gz
 tar -xvf install_flash_player_11_linux.x86_64.tar.gz
 sudo cp libflashplayer.so /usr/lib/mozilla/plugins
-
 cd /home
 wget 149.56.111.62/ch2.tar.gz
 tar -xvf ch2.tar.gz
@@ -57,7 +43,6 @@ tar -xvf Pulse.tar.gz
 #cp -r chrome /home/12
 #wget https://raw.githubusercontent.com/mazbron/x/master/every.tar.gz
 #tar -xvf every.tar.gz
-
 cd
 mkdir Desktop
 cd Desktop
@@ -65,7 +50,6 @@ wget https://github.com/dhorez/sempak/raw/master/Thormotorcoach.sh
 chmod +x Thormotorcoach.sh
 wget https://github.com/dhorez/sempak/raw/master/Victory.sh
 chmod +x Victory.sh
-
 #apt-get install proxychains -y
 #
 #apt-get install sl -y
